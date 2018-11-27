7d24c3f6e8f490a4fc04112c0aba18e12f9c7ef4
AIMS\rfisher <r.fisher@aims.gov.au>
2018-11-27 15:16:44 +0800

    Added function clean_tibble _colnames

--- R/clean_tibble_colnames.R
+++ R/clean_tibble_colnames.R
@@ -1,4 +1,16 @@
 
+
+#' clean.tibble.colnames
+#'
+#' Function to remove illegal characters in tibbles imported through tidyverse methods.
+#'
+#' @param  x The tibble or data.frame to clean
+#'
+#' @details Removes illegal characters in column names allowed in tibbles but would not normally be allowed in base R. this is to make them usable with code that odes not support this functionality of tibbles. Currently replaces " " with "." and all other characters with "".
+#'
+#' @export
+#' @return x with column names stripped of illegal characters.
+#'
 clean.tibble.colnames <- function(x){
    name.vals = names(x)
    name.vals <- gsub(" " , ".",name.vals,fixed=T)
