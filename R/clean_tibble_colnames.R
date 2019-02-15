2d2a34202504cdbbd18d056605f7be9f2fdc543e
beckyfisher <rebeccafisher76@gmail.com>
2019-02-15 15:37:41 +0800

    added cleaning of #, & and /

--- R/clean_tibble_colnames.R
+++ R/clean_tibble_colnames.R
@@ -22,6 +22,9 @@ clean.tibble.colnames <- function(x){
    name.vals <- gsub(")" , "",name.vals,fixed=T)
    name.vals <- gsub("%" , "",name.vals,fixed=T)
    name.vals <- gsub("-" , "",name.vals,fixed=T)
+   name.vals <- gsub("/" , "",name.vals,fixed=T)
+   name.vals <- gsub("#" , "",name.vals,fixed=T)
+   name.vals <- gsub("&" , "",name.vals,fixed=T)
    names(x)<-name.vals
    return(x)}
 
