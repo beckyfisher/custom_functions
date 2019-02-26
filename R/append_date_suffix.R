ec6aae7621731ea8a568692374fcfa561c1bb7d7
beckyfisher <rebeccafisher76@gmail.com>
2019-02-26 14:08:37 +0800

    Minor updates to documentation

--- R/append_date_suffix.R
+++ R/append_date_suffix.R
@@ -1,6 +1,6 @@
 #' append_date_suffix
 #'
-#' Calculates a model value, using the most appropriate method given the data type.
+#' Generates a day of the month value with the approriate suffix (e.g. 9th or 2nd)
 #' Obtained from https://stackoverflow.com/questions/40039903/r-add-th-rd-and-nd-to-dates
 #'
 #' @param  dates A vector of dates for which to generate the appropriate day of the month and assocaited suffix
