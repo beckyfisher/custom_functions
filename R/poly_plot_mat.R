748335c3f5f71d4a8ad31f8b88e678cf6dfb9ad5
beckyfisher <rebeccafisher76@gmail.com>
2019-05-07 08:18:36 +0800

    Separated functions into two scripts and corrected error. Troublshooting.

--- R/poly_plot_mat.R
+++ R/poly_plot_mat.R
@@ -1,21 +1,3 @@
-#' poly.plot.list
-#'
-#' Plot a list of values as polygons on the existing plot
-#'
-#' @param  x A list of x values for plotting - must contain two
-#' elements representing the start and end values of the polygons
-#'
-#' @param  y.lim The y values over which to draw the polygons
- poly.plot.list <- function(x,y.lim,alpha.f=0.3,col.val="grey"){
-   for(d in 1:length(x)){
-      x.d=x[[d]]
-      polygon(c(x.d[1],x.d[1],
-                x.d[2],x.d[2]),
-                c(y.lim, rev(y.lim)),
-               col=adjustcolor(col.val, alpha.f = alpha.f), border=NA)}
- }
-
-
 #' poly.plot.mat
 #'
 #' Plot polygons on the existing plot from a supplied matrix of values
